5d41bfd2860621f3c8deb349417a0a2ea6c865cf
user18081998 <Amustaphag22@gmail.com>
2020-04-19 00:15:29 +0100

    Update moteur.h

--- main/moteur.h
+++ main/moteur.h
@@ -44,62 +44,51 @@ class Request{
     public :
     Request(const vector<Occ>& v,const int& t=0);
     ~Request();
+    void trier();
 
     int total; // nombre total de resultats
     int size() const ; // nombre reel de resultats (size<=total comme on a une contrainte sur le nombre de resultats : MAX_N nombre total de resultats a afficher, voir class analyseur, voir index.searc)
 
+    vector<Occ>& getReq();
+
     friend ostream& operator<<(ostream& out,const Request& request);
 };
-class Index{
+class IndexUnorderedMap{
     unordered_map<string, vector<Occ> > dict;
 
     public:
-    Index();
-    ~Index();
+    IndexUnorderedMap();
+    ~IndexUnorderedMap();
 
     int size() const ; //nombre de mots indexes
 
     void indexer(const vector<Token>& tokens,const string& filename); // ajout dans l'index
-
-    friend ostream& operator<<(ostream& out,const Index& index);
+    vector<Occ> operator[](const string& s);
+    friend ostream& operator<<(ostream& out,const IndexUnorderedMap& index);
+};
+class IndexSet{
+    //...
 };
 class Analyseur{
-    Index index;
-    int MAX_N; // nombre maximum de resultats pour les requetes que l'analyseur affiche 
-    int size_; // nombre de fichiers dans l'analyseur
-    vector<Token> analyser(const string& filename);
-        /* analyser est une fonction qui accepte le nom d'un fichier
-       elle doit retourner un vecteur de tokens qu'on degage du fichier
-       un token est le pair {mot,n} 
-       avec n le nombre d'occurences du mot dans le fichier */
-    
     public:
-    Analyseur(const int& max=5);
-    Analyseur(const vector<string>& fileList,const int& max=5);
+    Analyseur();
     ~Analyseur();
-    
-    // la seule "raison d'etre" de ces deux methodes est d'afficher des statistiques concernant l'analyseur
-    int size() const ; //nombre de fichiers 
-    int indexSize() const ; //nombre de mots
 
-    
+    vector<Token> analyser(const vector<string>& text);
+
 };
 class Lecteur{
     public :
     vector<string> readFile(const string& chemin);
 };
-class Ordonnanceur{
-    public:
-    void trier(const Request& request);
-};
-class Moteur{
+template<class Index> class Moteur{
+    public :
+    Moteur();
+    ~Moteur();
     Index index;
     Analyseur analyseur;
     Lecteur lecteur;
-    Ordonnanceur ordonnanceur;
-    public :
     Request rechercher(const vector<string>& request);
-    void indexer(const vector<Token>& tokens,const string& chemin); 
-    // == index.indexer(const vector<Token>& tokens,const string& chemin)
+
 };
 #endif
